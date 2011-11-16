db61e2148544dfb931d7994d7f6250a5d9936397
Sebastian Tramp <mail@sebastian.tramp.name>
2011-11-16 22:39:16 +0100

    add webid to footer

--- script.js
+++ script.js
@@ -3,15 +3,18 @@
  * for Leipzig School of Media
  *
  * @author Michael Haschke, http://michael.haschke.biz/
- *
+ * @author Sebastian Tramp, http://sebastian.tramp.name
  */
 
 $(document).bind('deck.beforeInit', function(){
+    var author     = $('meta[name=author]').attr('content');
+    var webid      = $('meta[name=webid]').attr('content');
+    var authorLink = '<a href="' + webid + '">' + author + ' | ' + webid + '</a>';
 
-    $('.deck-meta-author').html($('meta[name=author]').attr('content'));
+    $('.deck-meta-author').html(authorLink);
     $('.deck-meta-date').html($('meta[name=date]').attr('content'));
     $('.deck-meta-title').html($('title').text());
-    
+
     $(document).bind('deck.change', function(event, from, to) {
         var currenttitle = $.deck('getSlide', to).find('h2').text();
         $('.title-from-current-deck').html(currenttitle);
